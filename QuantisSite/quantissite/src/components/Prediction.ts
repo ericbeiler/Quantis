58b43195d8bbaa83cd18fef0f0430877b7a5516d
Beiler, Eric <eric.beiler@gmail.com>
2024-12-14 21:48:09 -0600

    This has linting warnings, but seems to be working.

--- QuantisSite/quantissite/src/components/Prediction.ts
+++ QuantisSite/quantissite/src/components/Prediction.ts
@@ -1,12 +1,12 @@
-type Prediction = {
-  "Ticker": "string",
-  "StartingDate": "string",
-  "StartingPrice": "string",
-  "EndingDate": "string",
-  "PredictedEndingPrice": "string",
-  "ExpectedPriceRangeLow": "string",
-  "ExpectedPriceRangeHigh": "string",
-  "PredictedCagr": "string"
+interface Prediction {
+  Ticker: string;
+  StartingDate: string;
+  StartingPrice: string;
+  EndingDate: string;
+  PredictedEndingPrice: string;
+  ExpectedPriceRangeLow: string;
+  ExpectedPriceRangeHigh: string;
+  PredictedCagr: string
 }
 
 export default Prediction;
--- QuantisSite/quantissite/src/components/PredictionGrid2.tsx
+++ QuantisSite/quantissite/src/components/PredictionGrid2.tsx
@@ -3,35 +3,51 @@ import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import { ColDef } from 'ag-grid-community'; // Column Definition Interface
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional Theme applied to the Data Grid
+
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ModuleRegistry } from 'ag-grid-community';
+
 import Prediction from "./Prediction";
 
+ModuleRegistry.registerModules([ ClientSideRowModelModule ]);
 const serverUrl = import.meta.env.VITE_SERVER;
 
 const PredictionGrid2 = () => {
 
   // Row Data: The data to be displayed.
-  const [predictions, setRowData] = useState([
-    { Ticker: "Loading", model: "", StartingPrice: 0, EndingDate: false },
-  ]);
-
-  const fieldName = (name: keyof Prediction) => name;
+  const [predictions, setRowData] = useState<Prediction[]>([]);
   
   // Column Definitions: Defines the columns to be displayed.
-  const predictionColumns: ColDef[] = [
-    { field: fieldName('Ticker'), headerName: 'Ticker', width: 100 },
-    { field: fieldName('StartingDate'), headerName: 'Starting Date', width: 150 },
-    { field: fieldName('StartingPrice'), headerName: 'Starting Price', width: 150, },
-    { field: fieldName('EndingDate'), headerName: 'Ending Date', width: 150 },
-    { field: fieldName('PredictedEndingPrice'), headerName: 'Predicted Price', width: 150 },
-    { field: fieldName('ExpectedPriceRangeLow'), headerName: 'Expected Low Price', width: 150 },
-    { field: fieldName('ExpectedPriceRangeHigh'), headerName: 'Expected High Price', width: 150 },
-    { field: fieldName('PredictedCagr'), headerName: 'Predicted CAGR', width: 150 }
+  const predictionColumns: ColDef<Prediction>[] = [
+    { field: 'Ticker', headerName: 'Ticker', width: 100 },
+    { field: 'StartingDate', headerName: 'Starting Date', width: 150 },
+    { field: 'StartingPrice', headerName: 'Starting Price', width: 150, },
+    { field: 'EndingDate', headerName: 'Ending Date', width: 150 },
+    { field: 'PredictedEndingPrice', headerName: 'Predicted Price', width: 150 },
+    { field: 'ExpectedPriceRangeLow', headerName: 'Expected Low Price', width: 150 },
+    { field: 'ExpectedPriceRangeHigh', headerName: 'Expected High Price', width: 150 },
+    { field: 'PredictedCagr', headerName: 'Predicted CAGR', width: 150 }
   ];
   const [loading, setLoading] = useState(true);
+
+  const loadingPrediction: Prediction[] = [
+    {
+      Ticker: 'Loading...',
+      StartingDate: '',
+      StartingPrice: '',
+      EndingDate: '',
+      PredictedEndingPrice: '',
+      ExpectedPriceRangeLow: '',
+      ExpectedPriceRangeHigh: '',
+      PredictedCagr: ''
+    }
+  ]
 
   useEffect(() => {
     const fetchPredictions = async () => {
       try {
+        setRowData(loadingPrediction)
+
         // Fetch the data from the API
         const response = await fetch(`${serverUrl}api/Predictions/8?ticker=SPY`);
         const predictionArray = await response.json();
@@ -52,7 +68,7 @@ const PredictionGrid2 = () => {
     // wrapping container with theme & size
     <div
       className="ag-theme-quartz" // applying the Data Grid theme
-      style={{ height: 1000, width: 1000 }} // the Data Grid will fill the size of the parent container
+      style={{ height: 1000, width: 1400 }} // the Data Grid will fill the size of the parent container
     >
       <AgGridReact
         rowData={predictions}
@@ -65,45 +81,6 @@ const PredictionGrid2 = () => {
         )}
     </div>
   )
-
-  /*
-  useEffect(() => {
-    const fetchPredictions = async () => {
-      try {
-        // Fetch the data from the API
-        const response = await fetch(`${serverUrl}api/Predictions/8?ticker=SPY`);
-        const predictionArray = await response.json();
-
-        // Update state
-        setPredictions(predictionArray);
-      } catch (error) {
-        console.error("Error fetching predictions:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchPredictions();
-  }, []); // Empty dependency array means this runs once after the component mounts
-
-
-  return (
-    <div className="ag-theme-quartz" >
-      <h2>Predictions</h2>
-      {loading ? (
-        <p>Loading predictions...</p>
-      ) : (
-        <div>
-          <p>Prediction Count: {predictions.length}</p>
-          <AgGridReact
-              rowData={rowData}
-              columnDefs={colDefs}
-            />
-        </div>
-      )}
-    </div>
-  );
-  */
 };
 
 export default PredictionGrid2;
