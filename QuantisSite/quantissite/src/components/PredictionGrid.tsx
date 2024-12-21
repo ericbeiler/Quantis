2987b2ac4f0939c475697a2f586baea60a600b57
Beiler, Eric <eric.beiler@gmail.com>
2024-12-21 10:35:38 -0600

    Show PRICE and CAGR for multiple years (Trend)

--- QuantisSite/quantissite/src/components/PredictionGrid.tsx
+++ QuantisSite/quantissite/src/components/PredictionGrid.tsx
@@ -11,22 +11,67 @@ const serverUrl = import.meta.env.VITE_SERVER;
 const PredictionGrid = () => {
 
   // Formatter function: Formats a numeric value to 2 decimal places or returns a default string.
-//  const formatToPrice = (value?: number | null, defaultValue: string = 'N/A') =>
-//    value !== undefined && value !== null ? value.toFixed(2) : defaultValue;
+  const formatToPrice = (value?: number | null, defaultValue: string = 'N/A') =>
+    value !== undefined && value !== null ? value.toFixed(2) : defaultValue;
 
   // Row Data: The data to be displayed.
   const [predictions, setRowData] = useState<PredictionTrend[]>([]);
   
   // Column Definitions: Defines the columns to be displayed.
   const predictionColumns: ColDef<PredictionTrend>[] = [
-    { field: 'Ticker', headerName: 'Ticker', width: 100 }//,
-//    { field: 'PredictionPoints[0].StartingDate', headerName: 'Starting Date', width: 150 },
-//    { field: 'StartingPrice', headerName: 'Starting Price', width: 150, valueFormatter: params => formatToPrice(params?.data?.StartingPrice) },
+    { field: 'Ticker', headerName: 'Ticker', width: 100 },
+    {
+      field: 'predictionArray[0].PricePoints[0].StartingDate', headerName: 'Starting Date', width: 120,
+      valueGetter: params => params.data?.PricePoints?.[0]?.StartingDate 
+    },
+    {
+      field: 'StartingPrice', headerName: 'Starting Price', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[0]?.StartingPrice),
+      valueGetter: params => params.data?.PricePoints?.[0]?.StartingPrice 
+    },
+    {
+      field: 'PredictedEndingPrice', headerName: 'Year 1 Price', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[0]?.PredictedEndingPrice),
+      valueGetter: params => params.data?.PricePoints?.[0]?.PredictedEndingPrice
+    },
+    {
+      field: 'PredictedEndingPrice', headerName: 'Year 2 Price', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[1]?.PredictedEndingPrice),
+      valueGetter: params => params.data?.PricePoints?.[1]?.PredictedEndingPrice
+    },
+    {
+      field: 'PredictedEndingPrice', headerName: 'Year 3 Price', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[2]?.PredictedEndingPrice),
+      valueGetter: params => params.data?.PricePoints?.[2]?.PredictedEndingPrice
+    },
+    {
+      field: 'PredictedEndingPrice', headerName: 'Year 5 Price', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[3]?.PredictedEndingPrice),
+      valueGetter: params => params.data?.PricePoints?.[3]?.PredictedEndingPrice
+    },
+    {
+      field: 'PredictedCagr', headerName: 'Year 1 CAGR', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[0]?.PredictedCagr),
+      valueGetter: params => params.data?.PricePoints?.[0]?.PredictedCagr
+    },
+    {
+      field: 'PredictedCagr', headerName: 'Year 2 CAGR', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[1]?.PredictedCagr),
+      valueGetter: params => params.data?.PricePoints?.[1]?.PredictedCagr
+    },
+    {
+      field: 'PredictedCagr', headerName: 'Year 3 CAGR', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[2]?.PredictedCagr),
+      valueGetter: params => params.data?.PricePoints?.[2]?.PredictedCagr
+    },
+    {
+      field: 'PredictedCagr', headerName: 'Year 5 CAGR', width: 120,
+      valueFormatter: params => formatToPrice(params.data?.PricePoints?.[3]?.PredictedCagr),
+      valueGetter: params => params.data?.PricePoints?.[3]?.PredictedCagr
+    }
 //    { field: 'EndingDate', headerName: 'Ending Date', width: 150 },
-//    { field: 'PredictedEndingPrice', headerName: 'Predicted Price', width: 150, valueFormatter: params => formatToPrice(params?.data?.PredictedEndingPrice) },
 //    { field: 'ExpectedPriceRangeLow', headerName: 'Expected Low Price', width: 150, valueFormatter: params => formatToPrice(params?.data?.ExpectedPriceRangeLow) },
 //    { field: 'ExpectedPriceRangeHigh', headerName: 'Expected High Price', width: 150, valueFormatter: params => formatToPrice(params?.data?.ExpectedPriceRangeHigh) },
-//    { field: 'PredictedCagr', headerName: 'Predicted CAGR', width: 150, valueFormatter: params => formatToPrice(params?.data?.PredictedCagr) }
   ];
   const [loading, setLoading] = useState(true);
 
@@ -34,7 +79,7 @@ const PredictionGrid = () => {
     const fetchPredictions = async () => {
       try {
         // Fetch the data from the API
-        const response = await fetch(`${serverUrl}api/Predictions/5?ticker=SPY`);
+        const response = await fetch(`${serverUrl}api/Predictions/1?ticker=SPY`);
         const predictionArray = await response.json();
 
         // Update state
